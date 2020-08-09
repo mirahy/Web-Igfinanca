8f4df9541a7c40bcfaaaeaa4e7d0f23d405ecc05
mirahy <myrahy@gmail.com>
2020-08-09 02:24:01 -0400

    Arquivos criados atraves do make:entity para a entity caixa

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -29,6 +29,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\App\Repositories\TbProfileRepository::class, \App\Repositories\TbProfileRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\TbTypeLauncRepository::class, \App\Repositories\TbTypeLauncRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\TbTypeLaunchRepository::class, \App\Repositories\TbTypeLaunchRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\TbCaixaRepository::class, \App\Repositories\TbCaixaRepositoryEloquent::class);
         //:end-bindings:
     }
 }
