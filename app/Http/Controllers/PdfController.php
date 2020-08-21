d15c47f19aa05ef9e323a8de88da5a0c42c428f3
mirahy <myrahy@gmail.com>
2020-08-21 16:07:16 -0400

    retirando consultas diretas a tabela launch

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -4,27 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Entities\TbLaunch;
+use App\Services\TbLaunchService;
 use PDF;
 
 class PdfController extends Controller
 {
 
+    protected $serviceLaunch;
+
+    public function __construct(TbLaunchService $serviceLaunch)
+    {
+        
+        $this->serviceLaunch  = $serviceLaunch;
+    }
+
+    //gerar pdf de fechamentos
     public function closing_pdf(Request $request)
     {
-
+        
         $def = '%';
+        $tpCaixa    = 'Dízimos';
+        $month      = 'Agosto';
+        $year       = '2020';
 
-        $dados = (TbLaunch::query()
-            ->with('user')
-            ->with('caixa')
-            ->with('type_launch')
-            ->where([['idtb_type_launch', 'LIKE', $request->query('launch', $def)], ['status', 'LIKE', $request->query('status', $def)]]))->get();
+        $dados      = $this->serviceLaunch->find_Parameters($request);
 
-        $data = [0];
-        $year = 2020;
-        $tot = TbLaunch::where([['idtb_type_launch', 'LIKE', $request->query('launch', $def)], ['status', 'LIKE', $request->query('status', $def)]])->sum('value');
+        $request->request->add(['operation' => '1']);
+        $entries    = $this->serviceLaunch->sum($request);
 
-        $pdf = PDF::loadView('reports.closingPDF', compact('dados', 'data', 'year', 'tot'));
+        $request->request->add(['operation' => '2']);
+        $exits      = $this->serviceLaunch->sum($request);
+
+        $balance    = $entries - $exits;
+
+        $pdf = PDF::loadView('reports.closingPDF', compact('dados', 'month', 'year', 'entries', 'exits', 'balance', 'tpCaixa'));
 
         return $pdf->setPaper('a4')->stream('fechamento');
     }
