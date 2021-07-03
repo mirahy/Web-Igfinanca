c8eb5e1efe0bc2779ca176c9c0e3cacb41c72b67
Mirahy <myrahy@gmail.com>
2021-07-03 16:28:20 -0400

    Adicionando tab Geral napagina de entradas

--- resources/views/launch/launchs_e.blade.php
+++ resources/views/launch/launchs_e.blade.php
@@ -29,13 +29,14 @@
       <!-- Page Heading -->      
 
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Entradas</h1>
+        <h2 class="h3 mb-0 text-gray-800">Entradas</h2>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Gerar Relatório</a>
       </div>
 
         <ul class="nav nav-tabs">
             <li class="nav-item active"><a class="nav-link active" href="#tab_launch_d" role="tab"  data-toggle="tab">Dízimos</a></li>
             <li class="nav-item"><a class="nav-link" href="#tab_launch_o" role="tab"  data-toggle="tab">Ofertas</a></li>
+            <li class="nav-item"><a class="nav-link" href="#tab_launch_others" role="tab"  data-toggle="tab">Geral</a></li>
         </ul>
 
         <div class="tab-content">
@@ -105,6 +106,40 @@
                             </table>
                 </div>
             
+            </div>
+
+            <div id="tab_launch_others" class="tab-pane">
+                <div class="container-fluid">
+                    <h2 class="text-center"><strong>Geral</strong></h2>
+                        <a id="btn_add_launch_others" class="btn btn-primary my-1"><i class="fas fa-plus ">&nbsp Lançar</i></a>
+                            <table id="dt_launch_others" class="table table-striped table-hover table-responsive display nowrap" >
+                                <thead>
+                                    <tr >
+                                        <th>Tipo</th>
+                                        <th>Nome</th>
+                                        <th>Valor</th>
+                                        <th>Caixa</th>
+                                        <th>Tp Pagamento</th>
+                                        <th>Coleta</th>
+                                        <th>Status</th>
+                                        <th data-orderable="false" >Período</th>
+                                        <th>Status Período</th>
+                                        <th>Data lançamento</th>
+                                        <th>Data Ataulização</th>
+                                        <th data-orderable="false" >Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th colspan="8"  style=" text-align:right">Total:</th>
+                                        
+                                        <th></th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                </div>
             </div>
         </div>
 
