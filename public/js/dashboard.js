5c11a38f743e755d75865554c3bba60aa68d4be7
Mirahy <myrahy@gmail.com>
2021-08-16 19:38:56 -0400

    Teste dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,11 +4,20 @@ $(function () {
     var exit = 0;
     var entrie_o = 0;
     var init_value_o = 0;
-    var exit_o = 0;    
+    var exit_o = 0; 
+    
+    $.ajax(
+        entrada_dizimo('entrada_dizimo'),
+        saida_dizimo('saida_dizimo'),
+        saldo_inicial('saldo_inicial'),
+        entrada_ofertas('entrada_ofertas'),
+        saida_ofertas('saida_ofertas'),
+        set_values('set_values')
+    )
 
 
     //retorna valor de entradas dos dízimos
-    function entrada_dizimo(){
+    function entrada_dizimo(mensagem){
         $.ajax({
             type: "GET",
             url: "sum?status=1&operation=1&caixa=1&closing_status=1",
@@ -17,11 +26,13 @@ $(function () {
                 entrie = response;              
             }
         })
+        console.log(mensagem);
         return parseFloat(entrie);
+        
     }
 
     //retorna valor para de saidas dos dízimos
-    function saida_dizimo(){
+    function saida_dizimo(mensagem){
         $.ajax({
             type: "GET",
             url: "sum?status=1&operation=2&caixa=1&closing_status=1",
@@ -30,12 +41,13 @@ $(function () {
                 exit = response;            
             }
         })
+        console.log(mensagem);
         return parseFloat(exit);  
     } 
     
 
     //retorna saldo dos meses anteriores para somar a entrada das ofertas
-    function saldo_inicial(){
+    function saldo_inicial(mensagem){
         $.ajax({
             type: "GET",
             url: "init?status=1&caixa=2&closing_status=0",
@@ -44,13 +56,14 @@ $(function () {
                 init_value_o = response;
             }
         })
+        console.log(mensagem);
         return parseFloat(init_value_o);
     }
     
 
 
     //retorna valor entradas das ofertas
-    function entrada_ofertas(){
+    function entrada_ofertas(mensagem){
         $.ajax({
             type: "GET",
             url: "sum?status=1&operation=1&caixa=2&closing_status=1",
@@ -59,12 +72,13 @@ $(function () {
                 entrie_o = response;
             }
         })
+        console.log(mensagem);
         return parseFloat(entrie_o);
     }
     
 
     //retorna valor saídas de ofertas
-    function saida_ofertas(){
+    function saida_ofertas(mensagem){
         $.ajax({
             type: "GET",
             url: "sum?status=1&operation=2&caixa=2&closing_status=1",
@@ -73,32 +87,28 @@ $(function () {
                 exit_o = response;    
             }
         })
+        console.log(mensagem);
         return parseFloat(exit_o);
     }
 
     // setar valores nos cards
-    function set_values(){
+    function set_values(mensagem){
         $.ajax({
             success: function (response) {
-                $("#entries").html('R$' + number_format(entrada_dizimo(),2,',','.'));
-                $("#exits").html('R$' + number_format(saida_dizimo(),2,',','.'));
-                $("#balance").html('R$' + number_format(entrada_dizimo() - saida_dizimo(),2,',','.')); 
-                $("#entries_o").html('R$' + number_format(entrada_ofertas() + saldo_inicial(),2,',','.'));
-                $("#exits_o").html('R$' + number_format(saida_ofertas(),2,',','.'));
-                $("#balance_o").html('R$' + number_format(entrada_ofertas() + saldo_inicial() - saida_ofertas(),2,',','.'));
+                $("#entries").html('R$' + number_format(entrada_dizimo('Olá 1'),2,',','.'));
+                $("#exits").html('R$' + number_format(saida_dizimo('Olá 2'),2,',','.'));
+                $("#balance").html('R$' + number_format(entrada_dizimo('Olá 3') - saida_dizimo('Olá 4'),2,',','.')); 
+                $("#entries_o").html('R$' + number_format(entrada_ofertas('Olá 5') + saldo_inicial('Olá 6'),2,',','.'));
+                $("#exits_o").html('R$' + number_format(saida_ofertas('Olá 7'),2,',','.'));
+                $("#balance_o").html('R$' + number_format((entrada_ofertas('Olá 8') + saldo_inicial('Olá 9')) - saida_ofertas('Olá 10'),2,',','.'));
+                console.log(mensagem);
                 
             }
+            
         }); 
     }
 
-    $.ajax(
-        entrada_dizimo(),
-        saida_dizimo(),
-        saldo_inicial(),
-        entrada_ofertas(),
-        saida_ofertas(),
-        set_values()
-    )
+    
 
     //retorna lançamentos pendentes dos dízimos
     $.ajax({
