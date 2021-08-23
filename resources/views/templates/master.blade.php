a6012f724f943fce001f064251ba1d459ab558ac
mirahy <myrahy@gmail.com>
2021-08-23 15:51:53 -0400

    Atualizando tags html meta

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -6,8 +6,9 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="author" content="Mirahy Fonseca">
+  <meta name="keywords" content="Finanças, Tesouraria, Igrejas">
+  <meta name="description" content="Sistema de Controle Financeiro para Igrejas">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 
   <title>System Finance</title>
