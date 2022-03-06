bbe917789514eaa727eca75dc11dc7890fadfca2
Mirahy Fonseca <myrahy@gmail.com>
2022-03-06 01:07:58 -0400

    Exibir junto a entrada total, o total por tipo de pagamento, trazer anexo realtórios por tipo de pagamento

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -33,8 +33,11 @@ class PdfController extends Controller
         $period     = TbClosing::where('id', $request['reference_month'])->get();
         $numMes     = $this->serviceLaunch->number_month($period[0]['month'] );
         $nomeArq    = $numMes.' '.$tpCaixa[0]['name'] . ' - ' . $period[0]['month']. '_' .$period[0]['year'];
+        $base       = session()->get('name_base');
 
         $dados      = $this->serviceLaunch->find_Parameters($request);
+        $dados2     = $this->serviceLaunch->saldoTipoPagamento($request);
+        
         
         $request->request->add(['operation' => '1']);
         $entries    = $this->serviceLaunch->sum($request);
@@ -44,21 +47,39 @@ class PdfController extends Controller
         
         $request->request->remove('operation');
         $request->request->add(['closing_status' => 0]);
-        $startBalance = $request['caixa'] == 2 ? $this->serviceLaunch->saldo($request) : 0;
-        $entries = $entries + $startBalance;
+        $startBalance   = $request['caixa'] == 2 ? $this->serviceLaunch->saldo($request) : 0;
+        $entries        = $entries + $startBalance;
 
-        $balance    = $entries - $exits;
+        $balance        = $entries - $exits;
+
 
         //retorna a view
-        //return View('reports.closingPDF', compact('dados', 'period', 'entries', 'exits', 'balance', 'tpCaixa'));
+        // return View('reports.closingPDF', compact('dados', 'dados2', 'period', 'entries', 'exits', 'balance', 'tpCaixa'));
 
         //DomPDF
         // $pdf = PDF::loadView('reports.closingPDF', compact('dados', 'period', 'entries', 'exits', 'balance', 'tpCaixa'));
         // return $pdf->setPaper('a4')->stream($nomeArq.'.pdf');
 
-        //Snappy PDF
-        $pdf = SnappyPDF::loadView('reports.closingPDF', compact('dados', 'period', 'entries', 'exits', 'balance', 'tpCaixa'));
+        //SnappyPDF
+        $pdf = SnappyPDF::loadView('reports.closingPDF', compact('dados', 'dados2', 'period', 'entries', 'exits', 'balance', 'tpCaixa'))
+                            ->setPaper('a4')->setOption('footer-right', 'Página [page] de [topage]')->setOption('footer-left', '[date] - [time]')
+                            ->setOption('header-left', $base);
         return $pdf->inline($nomeArq.'.pdf');
+
+        /* ->setOption(SnappyPDF)
+        [page] Replaced by the number of the pages currently being printed
+        [frompage] Replaced by the number of the first page to be printed
+        [topage] Replaced by the number of the last page to be printed
+        [webpage] Replaced by the URL of the page being printed
+        [section] Replaced by the name of the current section
+        [subsection] Replaced by the name of the current subsection
+        [date] Replaced by the current date in system local format
+        [isodate] Replaced by the current date in ISO 8601 extended format
+        [time] Replaced by the current time in system local format
+        [title] Replaced by the title of the of the current page object
+        [doctitle] Replaced by the title of the output document
+        [sitepage] Replaced by the number of the page in the current site being converted
+        [sitepages] Replaced by the number of pages in the current site being converted */
 
         
     }
--- resources/views/reports/closingPDF.blade.php
+++ resources/views/reports/closingPDF.blade.php
@@ -9,118 +9,201 @@
 
   <!-- Custom styles for this template-->
   <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
 
   <div class="container" style="font-size: 0.8rem !important">
-    <h3 class="text-center"><strong>Fechamento de Caixa - {{$tpCaixa[0]['name']}}</strong></h3>
-    <h4 class="text-center"><strong>Período: {{$period[0]['month'].'/'.$period[0]['year']}}</strong></h4>
-    <div class="row col-6">
-      <table class="table table-borderless table-sm">
-        <thead>
-        </thead>
-        <tbody>
-          <tr class="table-secondary">
-            <td>
-              <!-- Entrada  -->
-              <div class="p-2">
-                <div class="text-xs font-weight-bold text-success text-uppercase">Entradas </div>
-                <div class=" font-weight-bold text-gray-800" id="entries">R$ {{ number_format($entries, 2, ',', '.') }}</div>
-              </div>
-            </td>
-            <td>
-              <!-- saídas -->
-              <div class="p-2">
-                <div class="text-xs font-weight-bold text-danger text-uppercase">Saídas</div>
-                <div class=" font-weight-bold text-gray-800" id="exits">R$ {{ number_format($exits, 2, ',', '.') }}</div>
-              </div>
-            </td>
-            <td>
-              <!-- Saldo Caixa  -->
-              <div class="p-2">
-                <div class="text-xs font-weight-bold text-info text-uppercase">Saldo</div>
-                <div class="font-weight-bold text-gray-800" id="balance">R$ {{ number_format($balance, 2, ',', '.') }}</div>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-        <tfoot></tfoot>
-      </table>
-    </div>
-      <table id="dt_report_closing" class="table ">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">Tipo</th>
-            <th scope="col">Nome</th>
-            <th scope="col" style="width: 35%;">Descrição</th>
-            <th scope="col" >Valor</th>
-            <th scope="col">Caixa</th>
-            <th scope="col">Tipo Pagamento</th>
-            <th scope="col">Data</th>
-          </tr>
-        </thead>
-        <tbody class="tb-size">
+    @if($dados2->count() <> 0)
+      <div class="page-break">
+        @endif
+        <h3 class="text-center"><strong>Fechamento de Caixa - {{$tpCaixa[0]['name']}}</strong></h3>
+        <h4 class="text-center"><strong>Período: {{$period[0]['month'].'/'.$period[0]['year']}}</strong></h4>
+        <div class="row col-6">
+          <table class="table table-borderless table-sm">
+            <thead>
+            </thead>
+            <tbody>
+              <tr class="table-secondary">
+                <td>
+                  <!-- Entrada  -->
+                  <div class="p-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase">Entradas </div>
+                    <div class=" font-weight-bold text-gray-800" id="entries">R$ {{ number_format($entries, 2, ',', '.') }}</div>
+                    <div style="font-size: 0.6rem !important">
+                      @foreach ($dados2 as $item)
+                      <div>{{$item->payment_type->name}} : R$ {{number_format( $item->total, 2, ',', '.')}}</div>
+                      @endforeach
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <!-- saídas -->
+                  <div class="p-2">
+                    <div class="text-xs font-weight-bold text-danger text-uppercase">Saídas</div>
+                    <div class=" font-weight-bold text-gray-800" id="exits">R$ {{ number_format($exits, 2, ',', '.') }}</div>
+                  </div>
+                </td>
+                <td>
+                  <!-- Saldo Caixa  -->
+                  <div class="p-2">
+                    <div class="text-xs font-weight-bold text-info text-uppercase">Saldo</div>
+                    <div class="font-weight-bold text-gray-800" id="balance">R$ {{ number_format($balance, 2, ',', '.') }}</div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot></tfoot>
+          </table>
+        </div>
+        <table id="dt_report_closing" class="table ">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Tipo</th>
+              <th scope="col">Nome</th>
+              <th scope="col" style="width: 35%;">Descrição</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Caixa</th>
+              <th scope="col">Tipo Pagamento</th>
+              <th scope="col">Data</th>
+            </tr>
+          </thead>
+          <tbody class="tb-size">
 
-          @foreach ($dados as $item)
-          <tr style="font-size: 0.7rem !important">
-            <th scope="row">{{ $item->type_launch->name }}</th>
-            <th>{{ $item->user->name }}</th>
-            <th style="font-size: 0.6rem !important">{{ $item->description }}</th>
-            <th class= @if ($item->idtb_operation == 2)
-              {{"text-danger"}}
-              @else
-              {{"text-success"}}
-              @endif>
-              {{'R$'}}
-              @if ($item->idtb_operation == 2)
-              {{'-'}}
-              @endif
-              {{number_format( $item->value, 2, ',', '.') }}
-            </th>
-            <th>{{ $item->caixa->name }}</th>
-            <th>{{ $item->payment_type->name }}</th>
-            <th>{{ $item->dataOperation }}</th>
-          </tr>
+            @foreach ($dados as $item)
+            <tr style="font-size: 0.7rem !important">
+              <th scope="row">{{ $item->type_launch->name }}</th>
+              <th>{{ $item->user->name }}</th>
+              <th style="font-size: 0.6rem !important">{{ $item->description }}</th>
+              <th class=@if ($item->idtb_operation == 2)
+                {{"text-danger"}}
+                @else
+                {{"text-success"}}
+                @endif>
+                {{'R$'}}
+                @if ($item->idtb_operation == 2)
+                {{'-'}}
+                @endif
+                {{number_format( $item->value, 2, ',', '.') }}
+              </th>
+              <th>{{ $item->caixa->name }}</th>
+              <th>{{ $item->payment_type->name }}</th>
+              <th>{{ $item->dataOperation }}</th>
+            </tr>
+            @endforeach
+
+          </tbody>
+          <tfoot>
+
+
+          </tfoot>
+        </table>
+        <div class="col-sm-12" style="font-size: 0.9rem !important">
+          <table class="table table-borderless table-sm">
+            <thead>
+            </thead>
+            <tbody>
+              <tr class="table-secondary">
+                <td>
+                  <div class="m-1">
+                    <div class="col-md-4">__________________</div>
+                    <div class="col-md-8 center">Tesouraria Sede</div>
+                  </div>
+                </td>
+                <td>
+                  <div class="m-1">
+                    <div class="col-md-4">__________________</div>
+                    <div class="col-md-8 center">Tesouraria Cong.</div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div class="col-md-4">__________________</div>
+                    <div class="col-md-8 center">Pastor Cong.</div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  @php($e = 1)
+                  @if($dados2->count() <> 0)
+                    <h6 class="m-2">Este relatório contem os seguintes anexos:</h6>
+                    @foreach($dados2 as $item1)
+                      <h6 class="m-2"> {{'Anexo '.$e.' - '.$item1->payment_type->name}} </h6>
+                      @php($e++)
+                    @endforeach
+                  @endif
+                </td>
+              </tr>
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+          </table>
+        </div>
+      </div>
+      <!-- Tabelas adicionais -->
+      @php($i = 1)
+      @if($dados2->count() <> 0)
+        @foreach($dados2 as $item1)
+
+        @if($i < $dados2->count())
+          <div class="page-break">
+            @endif
+            <h3 class="text-center"><strong>Fechamento de Caixa - {{$tpCaixa[0]['name']}}</strong></h3>
+            <h4 class="text-center"><strong>Período: {{$period[0]['month'].'/'.$period[0]['year']}}</strong></h4>
+            <h1> {{'Anexo '.$i.' - '.$item1->payment_type->name}} </h1>
+            <table id="dt_report_closing" class="table ">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Tipo</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col" style="width: 35%;">Descrição</th>
+                  <th scope="col">Valor</th>
+                  <th scope="col">Caixa</th>
+                  <th scope="col">Tipo Pagamento</th>
+                  <th scope="col">Data</th>
+                </tr>
+              </thead>
+              <tbody class="tb-size">
+
+                @foreach ($dados as $item)
+                @if($item->idtb_payment_type == $item1->payment_type->id)
+                <tr style="font-size: 0.7rem !important">
+                  <th scope="row">{{ $item->type_launch->name }}</th>
+                  <th>{{ $item->user->name }}</th>
+                  <th style="font-size: 0.6rem !important">{{ $item->description }}</th>
+                  <th class=@if ($item->idtb_operation == 2)
+                    {{"text-danger"}}
+                    @else
+                    {{"text-success"}}
+                    @endif>
+                    {{'R$'}}
+                    @if ($item->idtb_operation == 2)
+                    {{'-'}}
+                    @endif
+                    {{number_format( $item->value, 2, ',', '.') }}
+                  </th>
+                  <th>{{ $item->caixa->name }}</th>
+                  <th>{{ $item->payment_type->name }}</th>
+                  <th>{{ $item->dataOperation }}</th>
+                </tr>
+                @endif
+                @endforeach
+
+
+              </tbody>
+              <tfoot>
+
+
+              </tfoot>
+            </table>
+          </div>
+          @php($i++)
           @endforeach
-
-        </tbody>
-        <tfoot>
-
-
-        </tfoot>
-      </table>
-    <div class="col-sm-12" style="font-size: 0.9rem !important">
-      <table class="table table-borderless table-sm">
-        <thead>
-        </thead>
-        <tbody>
-          <tr class="table-secondary">
-            <td>
-              <div class="m-1">
-                <div class="col-md-4">__________________</div>
-                <div class="col-md-8 center">Tesouraria Sede</div>
-              </div>
-            </td>
-            <td>
-              <div class="m-1">
-                <div class="col-md-4">__________________</div>
-                <div class="col-md-8 center">Tesouraria Cong.</div>
-              </div>
-            </td>
-            <td >
-              <div>
-                <div class="col-md-4">__________________</div>
-                <div class="col-md-8 center">Pastor Cong.</div>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-        <tfoot></tfoot>
-      </table>
-    </div>
+          @endif
   </div>
   <!-- /.container-fluid -->
 
